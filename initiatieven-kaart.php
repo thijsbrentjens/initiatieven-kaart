2fb20fc3fd5c994f18c6bbf04cbb5fcc62360bba
Paul van Buuren <paul@wbvb.nl>
2020-10-08 21:00:39 +0200

    Custom Post Type en Taxonomie worden hier toegevoegd, strings vertaald

--- initiatieven-kaart.php
+++ initiatieven-kaart.php
@@ -1,21 +1,16 @@
 <?php
 
 /**
- * The plugin bootstrap file
+ * LED initiatieven-kaart
  *
- * This file is read by WordPress to generate the plugin information in the plugin
- * admin area. This file also includes all of the dependencies used by the plugin,
- * registers the activation and deactivation functions, and defines a function
- * that starts the plugin.
- *
- * @link              http://example.com
+ * @link              https://www.digitaleoverheid.nl/
  * @since             1.0.0
  * @package           Initiatieven_Kaart
  *
  * @wordpress-plugin
- * Plugin Name:       Initiatieven Kaart
+ * Plugin Name:       Initiatieven Kaart voor LED (digitaleoverheid.nl)
  * Plugin URI:        http://example.com/initiatieven-kaart-uri/
- * Description:       Toont LED initiatieven op een
+ * Description:       Toont LED initiatieven op een kaart
  * Version:           1.0.0
  * Author:            Thijs Brentjens
  * Author URI:        https://brentjensgeoict.nl
@@ -43,6 +38,8 @@ define( 'INITIATIEVEN_KAART_VERSION', '1.0.0' );
  */
 require plugin_dir_path( __FILE__ ) . 'includes/class-initiatieven-kaart.php';
 
+//========================================================================================================
+
 /**
  * Begins execution of the plugin.
  *
@@ -61,15 +58,109 @@ function run_initiatieven_kaart() {
 
 
 }
+
 run_initiatieven_kaart();
 
-// Thijs: setting up things, for dev. TODO: better place?
+//========================================================================================================
+
 function get_custom_post_type_template( $archive_template ) {
 	global $post;
 
 	if ( is_post_type_archive ( CPT_INITIATIEF ) ) {
+		// het is een archive voor CPT = CPT_INITIATIEF
 		$archive_template = dirname( __FILE__ ) . '/includes/templates/archive-initiatieven.php';
 	}
+
 	return $archive_template;
+
 }
+
 add_filter( 'archive_template', 'get_custom_post_type_template' ) ;
+
+//========================================================================================================
+
+function wporg_custom_post_type() {
+
+
+	$args = array(
+		'label'               => esc_html__( CPT_INITIATIEF, 'waymark' ),
+		'description'         => '',
+		'labels'              => array(
+			'name'                  => esc_html_x( 'Initiatieven', 'post type', 'initiatieven-kaart' ),
+			'singular_name'         => esc_html_x( 'Initiatief', 'post type', 'initiatieven-kaart' ),
+			'menu_name'             => esc_html_x( 'Initiatieven', 'post type', 'initiatieven-kaart' ),
+			'name_admin_bar'        => esc_html_x( 'Initiatief', 'post type', 'initiatieven-kaart' ),
+			'archives'              => esc_html_x( 'Overzicht initiatieven', 'post type', 'initiatieven-kaart' ),
+			'attributes'            => esc_html_x( 'Eigenschappen initiatief', 'post type', 'initiatieven-kaart' ),
+			'parent_item_colon'     => esc_html_x( 'Parent Map:', 'post type', 'initiatieven-kaart' ),
+			'all_items'             => esc_html_x( 'Alle initiatieven', 'post type', 'initiatieven-kaart' ),
+			'add_new_item'          => esc_html_x( 'Initiatief toevoegen', 'post type', 'initiatieven-kaart' ),
+			'add_new'               => esc_html_x( 'Toevoegen', 'post type', 'initiatieven-kaart' ),
+			'new_item'              => esc_html_x( 'Nieuw initiatief', 'post type', 'initiatieven-kaart' ),
+			'edit_item'             => esc_html_x( 'Bewerk initiatief', 'post type', 'initiatieven-kaart' ),
+			'update_item'           => esc_html_x( 'Update initiatief', 'post type', 'initiatieven-kaart' ),
+			'view_item'             => esc_html_x( 'Bekijk initiatief', 'post type', 'initiatieven-kaart' ),
+			'view_items'            => esc_html_x( 'Bekijk initiatieven', 'post type', 'initiatieven-kaart' ),
+			'search_items'          => esc_html_x( 'Zoek initiatief', 'post type', 'initiatieven-kaart' ),
+			'not_found'             => esc_html_x( 'Not found', 'post type', 'initiatieven-kaart' ),
+			'not_found_in_trash'    => esc_html_x( 'Not found in Trash', 'post type', 'initiatieven-kaart' ),
+			'featured_image'        => esc_html_x( 'Featured Image', 'post type', 'initiatieven-kaart' ),
+			'set_featured_image'    => esc_html_x( 'Set featured image', 'post type', 'initiatieven-kaart' ),
+			'remove_featured_image' => esc_html_x( 'Remove featured image', 'post type', 'initiatieven-kaart' ),
+			'use_featured_image'    => esc_html_x( 'Use as featured image', 'post type', 'initiatieven-kaart' ),
+			'insert_into_item'      => esc_html_x( 'Insert into Map', 'post type', 'initiatieven-kaart' ),
+			'uploaded_to_this_item' => esc_html_x( 'Uploaded to this initiatief', 'post type', 'initiatieven-kaart' ),
+			'items_list'            => esc_html_x( 'Map list', 'post type', 'initiatieven-kaart' ),
+			'items_list_navigation' => esc_html_x( 'Maps list navigation', 'post type', 'initiatieven-kaart' ),
+			'filter_items_list'     => esc_html_x( 'Filter initiatief list', 'post type', 'initiatieven-kaart' ),
+		),
+		'supports'            => array( 'title', 'author', 'excerpt', 'editor' ),
+		'hierarchical'        => false,
+		'public'              => true,
+		'show_ui'             => true,
+		'show_in_menu'        => true,
+		'show_in_admin_bar'   => true,
+		'show_in_nav_menus'   => true,
+		'can_export'          => true,
+		'has_archive'         => true,
+		'exclude_from_search' => false,
+		'publicly_queryable'  => true,
+		'rewrite'             => array( 'slug' => CPT_INITIATIEF ),
+		'capability_type'     => 'post'
+	);
+
+	register_post_type( CPT_INITIATIEF, $args );
+
+	// Add new taxonomy, make it hierarchical (like categories)
+	$labels = array(
+		'name'              => esc_html_x( 'Type initatief', 'taxonomy', 'initiatieven-kaart' ),
+		'singular_name'     => esc_html_x( 'Initatieftype', 'taxonomy singular name', 'initiatieven-kaart' ),
+		'search_items'      => esc_html_x( 'Search initatieftype', 'taxonomy', 'initiatieven-kaart' ),
+		'all_items'         => esc_html_x( 'All initatieftypes', 'taxonomy', 'initiatieven-kaart' ),
+		'parent_item'       => esc_html_x( 'Parent initatieftype', 'taxonomy', 'initiatieven-kaart' ),
+		'parent_item_colon' => esc_html_x( 'Parent initatieftype:', 'taxonomy', 'initiatieven-kaart' ),
+		'edit_item'         => esc_html_x( 'Edit initatieftype', 'taxonomy', 'initiatieven-kaart' ),
+		'update_item'       => esc_html_x( 'Update initatieftype', 'taxonomy', 'initiatieven-kaart' ),
+		'add_new_item'      => esc_html_x( 'Add New initatieftype', 'taxonomy', 'initiatieven-kaart' ),
+		'new_item_name'     => esc_html_x( 'New initatieftype Name', 'taxonomy', 'initiatieven-kaart' ),
+		'menu_name'         => esc_html_x( 'Initatieftype', 'taxonomy', 'initiatieven-kaart' ),
+	);
+
+	$args = array(
+		'hierarchical'      => true,
+		'labels'            => $labels,
+		'show_ui'           => true,
+		'show_admin_column' => true,
+		'query_var'         => true,
+		'rewrite'           => array( 'slug' => CT_INITIATIEFTYPE ),
+	);
+
+	register_taxonomy( CT_INITIATIEFTYPE, array( CPT_INITIATIEF ), $args );
+	
+	
+}
+
+add_action('init', 'wporg_custom_post_type');
+
+//========================================================================================================
+
