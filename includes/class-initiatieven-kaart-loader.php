7a2c794b7d5b8aa82ffdb5430a3faf24aaa27e35
Thijs Brentjens <thijs@brentjensgeoict.nl>
2021-02-17 12:44:16 +0100

    Code opschonen: een andere URI dan example.com. Al weet ik nog geen betere...

--- includes/class-initiatieven-kaart-loader.php
+++ includes/class-initiatieven-kaart-loader.php
@@ -3,7 +3,7 @@
 /**
  * Register all actions and filters for the plugin
  *
- * @link       http://example.com
+ * @link       https://digitaleoverheid.nl
  * @since      1.0.0
  *
  * @package    Initiatieven_Kaart
